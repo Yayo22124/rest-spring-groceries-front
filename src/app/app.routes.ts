d47ae9f1d8c1d5ce2d10c77d08f2433998f36c1c
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-28 07:06:25 -0600

    Add: Architecture and route configuration for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Redirect to default Path
+    {
+        path: "",
+        pathMatch: 'full',
+        redirectTo: "/groceries/home"
+    },
+    {
+        path: "groceries",
+        children: [
+            { //! Default Route
+                path: "home",
+                component: HomeComponent
+            },
+            {
+                path: "employees",
+                loadComponent: () => import("./pages").then(comp => comp.EmployeesComponent)
+            },
+            {
+                path: "products",
+                loadComponent: () => import("./pages").then(comp => comp.ProductsComponent)
+            },
+            {
+                path: "suppliers",
+                loadComponent: () => import("./pages").then(comp => comp.SuppliersComponent)
+            },
+            {
+                path: "sales",
+                loadComponent: () => import("./pages").then(comp => comp.SalesComponent)
+            },
+            {
+                path: "orders",
+                loadComponent: () => import("./pages").then(comp => comp.OrdersComponent)
+            },
+            {
+                path: "customers",
+                loadComponent: () => import("./pages").then(comp => comp.CustomersComponent)
+            }            
+        ]
+    }
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,7 @@
+export * from "./customers/customers.component";
+export * from "./employees/employees.component";
+export * from "./employees/employees.component";
+export * from "./orders/orders.component";
+export * from "./products/products.component";
+export * from "./sales/sales.component";
+export * from "./suppliers/suppliers.component";
